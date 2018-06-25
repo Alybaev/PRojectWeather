8be8274e42fcc1a10eb73fb3069790543de0e0e3
Dosya <alybaev_d@auca.kg>
2018-06-25 17:19:38 +0600

    Begin test

--- app/src/androidTest/java/com/example/admin/weather/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/admin/weather/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.example.admin.weather
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.closeSoftKeyboard
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,9 +24,19 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    val mActivityRule = ActivityTestRule(
+            MainActivity::class.java)
+
+
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
+
+        onView(withId(R.id.ed))
+                .perform(typeText("njn"), closeKeybord());
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.example.admin.weather", appContext.packageName)
     }
